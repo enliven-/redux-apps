4c3dc74f938cd73d4c60af75be769ff78ece755e
Viksit Arora <viksit@mit.edu>
2016-06-09 11:09:48 -0400

    added reducers

---
+++ todos/reducers/todos.js
@@ -0,0 +1,21 @@
+import todo from './todo';
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, todo(undefined, action)];
+
+    case 'TOGGLE_TODO':
+      return state.map(t => {
+        if (action.id != t.id) { 
+          return t; 
+        }
+        return todo(t, action);
+      });
+
+    default:
+      return state;
+  }
+}
+
+export default todos;
